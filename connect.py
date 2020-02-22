41267566ef5522e9ddbddad939a691c31b4b4e20
unknown <csstransky@gmail.com>
2020-02-22 18:24:10 -0500

    Added instructions to file

--- connect.py
+++ connect.py
@@ -10,13 +10,24 @@ buffsz = 2048
 
 client.connect((fbrain, 55557))
 
-while ( True ):
-	print( '<< receiving data' )
-	response = client.recv(buffsz).decode('utf-8')
-	print( response )
+# 1. Connect the Raspberry Pi to the fish across WM-003
+# 2. Connect the laptop to the fish across WM-003
+# 3. Open the OCU (C:/program_files(x86)/AUV/BIOSwimmerOCU.exe) on the laptop
+# 4. In the OCU, zero out the fins and tails
+# 5. Click on the "Manual Mode" tab, then press the "Manual Mode Enable" once
+# 6. In the top right, it should switch from "Setup" to "Operate (PDO)"
+# 7. Now run the Python script to move and read the fish
 
-	print( '>> send test message' )
-	client.send("{vNorth: 0.43, vEast: 1.56, depth: 0.56}".encode())
-	# client.send("{vNorth: 0.43, vEast: 1.56, depth: 0.56, TailAngleEnable:True, TailAngle: 3, SpeedOverrideEnable:True, SpeedOver: 0.1}".encode())
+try: 
+	while ( True ):
+		print( '<< receiving data' )
+		response = client.recv(buffsz).decode('utf-8')
+		print( response )
 
+		print( '>> send test message' )
+		# client.send("{vNorth: 0.43, vEast: 1.56, depth: 0.56}".encode())
+		client.send("{vNorth: 0.43, vEast: 1.56, depth: 0.56, TailAngleEnable:True, TailAngle: 3, SpeedOverrideEnable:True, SpeedOver: 0.2}".encode())
+except KeyboardInterrupt:
+	client.close()
+	print('interrupted!')
 	
