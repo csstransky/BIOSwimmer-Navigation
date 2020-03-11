f68c85512b42fd4365febcc5090d9ffcdab9f498
unknown <csstransky@gmail.com>
2020-03-11 16:20:38 -0400

    work from 3-11-2020; 4:20pm

--- read_data.py
+++ read_data.py
@@ -1,8 +1,21 @@
 
 import re
 
+def update_bioswimmer(bioswimmer, command_columns):
+    command = command_columns[0]
+    # TODO: Continue work from here, I was creating a switch case to handle each command
+    return {
+        'a': 1,
+        'b': 2
+    }.get(x, 9)
+    return
+
 def read_bioswimmer_data(bioswimmer, serial_data):
     print(serial_data)
     commands = re.findall(r'\{([^}]+)', serial_data)
     print(commands)
+    for command in commands:
+        print(command)
+        command_columns = re.split(r'\W+', command)
+        print(command_columns)
     return
