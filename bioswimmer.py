92b033f25617d5be376add2828c1a8356c24aff4
unknown <csstransky@gmail.com>
2020-03-11 16:06:28 -0400

    Basic bioswimmer class structure

--- bioswimmer.py
+++ bioswimmer.py
@@ -4,4 +4,15 @@ class BIOSwimmer:
         self.x = 0
         self.y = 0
         self.z = 0
-        self.compass = 0
+        self.comp_pitch = 0
+        self.comp_roll = 0
+        self.comp_dir = 0
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.veloctiy_z = 0
+        self.acc_x = 0
+        self.acc_y = 0
+        self.acc_z = 0
+        self.seafloor_dist = 0
+
+    
