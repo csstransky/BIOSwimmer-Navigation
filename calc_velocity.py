e7136129f3f39fb266854b5aafe812d505787f65
bazooka <csstransky@gmail.com>
2020-04-01 14:51:50 -0400

    Remade velocity algorithm

--- calc_velocity.py
+++ calc_velocity.py
@@ -1,6 +1,6 @@
 # NOTE: Assuming an accerlation of 1 m/s^2 is ideal, may need to be lowered in the future to
 # keep up with the BIOSwimmer's slow rate of change in the rotator
-SPEED_RATE = 1
+IDEAL_ACCELERATION = 1
 
 # TODO: This math is completely theoretical and untested, this will need to be edited and tested 
 # in the future
@@ -8,7 +8,7 @@ SPEED_RATE = 1
 def get_new_velocity_double(destination_location, 
     current_location, current_velocity, current_accerlation):
 
-    ideal_velocity = SPEED_RATE * (destination_location - current_location)
-    needed_velocity = ideal_velocity - current_velocity
-    needed_acceleration = SPEED_RATE - current_velocity
-    return needed_velocity + needed_acceleration
+    ideal_velocity = IDEAL_ACCELERATION * (destination_location - current_location)
+    needed_acceleration = IDEAL_ACCELERATION - current_accerlation
+    needed_velocity = needed_acceleration * (ideal_velocity - current_velocity)
+    return needed_velocity
